8e7594c3ded97be8f9e9bb8665eaa7ab0d95163c
Samuel Thompson <thomps9012@gmail.com>
2023-03-04 13:53:37 -0500

    removed comments

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,23 +10,15 @@ const config = {
   tagline: "The russian dog says гав",
   favicon: "img/logo.png",
 
-  // Set the production url of your site here
   url: "https://thomps9012.github.io",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "thomps9012", // Usually your GitHub org/user name.
-  projectName: "script_sweep", // Usually your repo name.
+  organizationName: "thomps9012",
+  projectName: "script_sweep",
   deploymentBranch: "docs",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   trailingSlash: false,
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
@@ -39,15 +31,9 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl: "https://github.com/thomps9012/script_sweep",
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl: "https://github.com/thomps9012/script_sweep",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -59,7 +45,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
       image: "img/logo.png",
       navbar: {
         title: "script_sweep",
