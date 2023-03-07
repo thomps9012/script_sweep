1dce896abae13e2ba2eebc7b8497409046e6e443
Samuel Thompson <thomps9012@gmail.com>
2023-03-07 12:33:08 -0500

    added in cors

--- app.ts
+++ app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import api from "./routes";
+import cors from "cors";
 const app: express.Application = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", api);
 export default app;
